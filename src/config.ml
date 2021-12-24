02c59bd218b873d2da7f8bac9e23f03ef027f45f
Mads Hartmann <mads379@gmail.com>
2021-12-24 11:32:22 +0000

    Formatting

--- src/config.ml
+++ src/config.ml
@@ -1,9 +1,6 @@
 open Mirage
 
 let main =
-  foreign
-    ~packages:[package "duration"]
-    "Unikernel.Hello" (time @-> job)
+  foreign ~packages:[ package "duration" ] "Unikernel.Hello" (time @-> job)
 
-let () =
-  register "hello" [main $ default_time]
+let () = register "hello" [ main $ default_time ]
